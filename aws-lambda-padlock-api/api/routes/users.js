fa8e870497f7d342ff38adcac07a5426e8815dc6
AlexGA93 <alexgimat@hotmail.com>
2021-05-07 16:20:40 +0200

    importing and exporting

--- aws-lambda-padlock-api/api/routes/users.js
+++ aws-lambda-padlock-api/api/routes/users.js
@@ -1,10 +1,14 @@
 // importing models
-import User from "../../models/user";
+//import User from "../../models/user";
 
 
 
 //@route POST api/users
 //@desc Register user
 //@access Public
+const hi = () => {
+    return "heyyyyyyyyyyyyyyyyyy";
+}
 
 
+module.exports = hi;
--- aws-lambda-padlock-api/handler.js
+++ aws-lambda-padlock-api/handler.js
@@ -1,22 +1,34 @@
 'use strict';
-const nodes = require('./api/routes/nodes');
+
+const hey = require('./api/routes/users');
+
 
 module.exports.padlock = async (event, context, callback) => {
   console.log(event);
   let method = event.requestContext.http.method; // GET, POST, PUT, UPDATE
   let identify = JSON.parse(event.body); // user or service 
-  nodes(method,identify.type);
   
   
-  //let body = JSON.parse(event.body);
-  //console.log(body));// extracting data as json from json-stringified(lol)
-  //let {name, email, password, age} = body;
-
+  console.log(hey);
+  let cosa = " "+nodes(method,identify.type)+" <---> "+hey;
 
   return callback(null, {
     statsCode: 200,
-    body: identify.type}
+    body: cosa}
   );
 };
 
 
+const nodes = (method,identify) =>{
+  // if identify is user or not
+  if(identify === 'user'){
+      console.log('is a user'+method);
+      return `user: ${identify} & method: ${method}`
+  }else if(identify === 'service'){
+      console.log('is a service');
+      return `service: ${identify} & method: ${method}`
+  }else{ 
+      console.log('is none of them');
+      return "none of them";
+  }
+}
