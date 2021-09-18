8bd7db599e9e8627288c50d37314c294a6eb7a5c
AlexGA93 <alexgimat@hotmail.com>
2021-09-18 15:19:28 +0200

    Defining reducers

--- padlock/src/reducers/service.js
+++ padlock/src/reducers/service.js
@@ -1,11 +1,44 @@
 // import action types
 
 //define an initial state
-const initialState = {};
+const initialState = {
+    service: null,
+    services: [],
+    loading: true,
+    error: {}
+  };
 
+/*
 
+GET_SERVICES
+*/
 const service = (state=initialState, action) => {
-    switch(action.type){
+    const {type, payload} = action;
+
+    switch(type){
+        case "GET_SERVICE":
+        case "UPDATE_SERVICE":
+            return {
+                ...state,
+                service: payload,
+                loading: false,
+                error:{}
+            }
+        case"GET_SERVICES":
+            return {
+                ...state,
+                services:payload,
+                loading:false,
+                error:{}
+            }
+        case "SERVICE_DELETED":
+        case "SERVICE_ERROR":
+            return {
+                ...state,
+                service:null,
+                loading:false,
+                services:[],
+            }
         default:
             return state;
     }
